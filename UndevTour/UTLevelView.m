9d28b3bf116475d4c6f7376b5cfead33ace148a3
Ivan Ushakov <Ivan.MoLoT@gmail.com>
2013-03-16 21:57:49 +0400

    add icons and zoom

--- UndevTour/UTLevelView.m
+++ UndevTour/UTLevelView.m
@@ -8,25 +8,57 @@
 
 #import "UTLevelView.h"
 #import "UTLevel.h"
+#import "UTExhibit.h"
 
 @implementation UTLevelView
+{
+    UIView *_container;
+    UIView *_iconsLayer;
+    UIView *_pathLayer;
+}
 
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.scrollEnabled = YES;
         self.backgroundColor = [UIColor darkGrayColor];
+        self.delegate = self;
     }
     return self;
 }
 
 - (void)setLevel:(UTLevel *)level {
     [[self subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    
+    _container = [[UIView alloc] init];
+    [self addSubview:_container];
+
     UIImage *mapImage = [UIImage imageNamed:level.imagePath];
     UIImageView *mapImageView = [[UIImageView alloc] initWithImage:mapImage];
-    [self addSubview:mapImageView];
+    [_container addSubview:mapImageView];
+
+    _container.frame = RectSetSize(_container.frame, mapImage.size.width, mapImage.size.height);
     self.contentSize = mapImage.size;
+    self.maximumZoomScale = 1.0f;
+    self.minimumZoomScale = 320.0f / mapImage.size.width;
     
+    _pathLayer = [[UIView alloc] init];
+    [_container addSubview:_pathLayer];
+    
+    _iconsLayer = [[UIView alloc] init];
+    [_container addSubview:_iconsLayer];
+    
+    for (UTExhibit *exhibit in level.exhibits) {
+        UIImageView *icon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:exhibit.iconPath]];
+        icon.frame = RectSetOrigin(icon.frame, [exhibit.coordinate CGPointValue].x, [exhibit.coordinate CGPointValue].y);
+        [_iconsLayer addSubview:icon];
+    }
+}
+
+#pragma marl - scroll view delegate
+
+- (UIView*)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return _container;
 }
 
 @end
