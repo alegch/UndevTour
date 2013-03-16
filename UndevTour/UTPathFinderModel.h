294b44628a6012d822563b651e64bf8d593dbe37
Ivan Ushakov <Ivan.MoLoT@gmail.com>
2013-03-16 13:47:29 +0400

    done blocks service

---
+++ UndevTour/UTPathFinderModel.h
@@ -0,0 +1,14 @@
+//
+//  UTPathFinderModel.h
+//  UndevTour
+//
+//  Created by Иван Ушаков on 16.03.13.
+//  Copyright (c) 2013 Chebulaev Oleg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UTPathFinderModel : NSObject
+
+
+@end
